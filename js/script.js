ab7f8756ae34ecabff8870dbcf0d24e3d337aa30
Zheka <grushevskiy.yevgeniy@mail.ru>
2022-11-02 14:14:54 +0300

    feat: add function to changed language

---
+++ js/script.js
@@ -0,0 +1,77 @@
+const getResource = async (url) => {
+    const res = await fetch(url);
+
+    if (!res.ok) {
+        throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
+
+    return await res.json();
+};
+
+
+
+class ChoiceLanguage {
+    constructor(profile, profileDescr, skills, firstname, speciality, education, works) {
+        this.profile = profile;
+        this.profileDescr = profileDescr;
+        this.skills = skills;
+        this.firstname = firstname;
+        this.speciality = speciality;
+        this.education = education;
+        this.works = works;
+    }
+
+    translate() {
+        const profile = document.querySelector('.left-content h2'),
+            profileDescr = document.querySelector('.left-content p'),
+            skills = document.querySelector('.left-content .title_indented h2'),
+            firstname = document.querySelector('.right-content h1'),
+            speciality = document.querySelector('.right-content h3'),
+            education = document.querySelector('.ed'),
+            works = document.querySelector('.work');
+        profile.innerHTML = this.profile;
+        profileDescr.innerHTML = this.profileDescr;
+        skills.innerHTML = this.skills;
+        firstname.innerHTML = this.firstname;
+        speciality.innerHTML = this.speciality;
+        education.innerHTML = this.education;
+        works.innerHTML = this.works;
+    }
+}
+
+
+
+const language = document.querySelector(".language");
+
+
+language.addEventListener("click", (e) => {
+    const ru = language.querySelector(".ru"),
+        eng = language.querySelector(".eng");
+
+    if (e.target.classList.contains("ru") || e.target.classList.contains("eng")) {
+
+        if (e.target == ru) {
+            eng.classList.remove("active")
+            ru.classList.add("active")
+        } else {
+            ru.classList.remove("active");
+            eng.classList.add("active");
+        }
+
+        const lng = e.target.classList.contains("ru") ? "ru" : "eng";
+        getResource(`../${lng}.json`)
+            .then(data => {
+                let {
+                    profile,
+                    profileDescr,
+                    skills,
+                    firstname,
+                    speciality,
+                    education,
+                    works
+                } = data;
+
+                new ChoiceLanguage(profile, profileDescr, skills, firstname, speciality, education, works).translate();
+            });
+    }
+});
